772a1c570c63d87418393bbd60c87f7545b169ae
RockyTaco <115428223+RockyTaco@users.noreply.github.com>
2024-01-28 15:52:38 -0500

    Modified index.html to have faculty on duty
    and added events route to app,py

--- app.py
+++ app.py
@@ -15,6 +15,9 @@ dorm_data = { #made data all stored in this dictionary for ease of change
 @app.route('/')
 def home():
     return render_template('index.html', dorm_data=dorm_data)
+@app.route('/events')
+def events():
+    return render_template('events.html', dorm_data=dorm_data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
